271c9ca24e1169f7a99afdfad81097ef5d846d6f
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-10-01 12:06:34 +0700

    3- add main components

---
+++ front-end/src/components/BookForm/BookForm.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './BookFrom.css'
+
+function BookForm() {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const handleSunbmit = (e) => {
+    e.prevenDefault()
+    if (title && author) {
+      //dispatch action
+      console.log(title, author)
+      setTitle('')
+      setAuthor('')
+    }
+  }
+  return (
+    <div className='app-block book-form'>
+      <h2>Add a New Book</h2>
+      <form onSubmit={handleSunbmit}>
+        <div>
+          <label htmlFor='title'>Title</label>
+          <input type='text' id='title' value={title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor='author'>Author</label>
+          <input
+            type='text'
+            id='author'
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
+        <button type='submit'>Add Book</button>
+      </form>
+    </div>
+  )
+}
+
+export default BookForm
