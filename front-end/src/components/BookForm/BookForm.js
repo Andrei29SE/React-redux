0281a3cded38a0413eda5b08510b510beac17af1
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-10-03 13:51:05 +0700

    7 - refactorinf for adding books and adding favourite

--- front-end/src/components/BookForm/BookForm.js
+++ front-end/src/components/BookForm/BookForm.js
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addBook } from '../../redux/books/actionCreators'
 import './BookFrom.css'
-import { v4 as uuidv4 } from 'uuid'
 import booksData from '../../data/books.json'
+import createBookWithId from '../../utils/createBookWithId'
 
 function BookForm() {
   const [title, setTitle] = useState('')
@@ -13,26 +13,13 @@ function BookForm() {
   const handleOnRandom = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length)
     const randomBook = booksData[randomIndex]
-
-    const randomBookWithId = {
-      ...randomBook,
-      id: uuidv4(),
-      isFavourite: false,
-    }
-    dispatch(addBook(randomBookWithId))
+    dispatch(addBook(createBookWithId(randomBook)))
   }
   const handleSunbmit = (e) => {
     e.preventDefault()
 
     if (title && author) {
-      const book = {
-        title: title,
-        author: author,
-        id: uuidv4(),
-        isFavourite: false,
-      }
-
-      dispatch(addBook(book))
+      dispatch(addBook(createBookWithId({ title, author })))
       setTitle('')
       setAuthor('')
     }
