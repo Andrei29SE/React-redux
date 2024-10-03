201c356dce4e4ef77a2b8e5a9a8fb58560900441
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-10-03 17:58:08 +0700

    8 - added filter component with slices approach

--- front-end/src/components/Filter/Filter.js
+++ front-end/src/components/Filter/Filter.js
@@ -1,9 +1,24 @@
 import './Filter.css'
+import { setTitleFilter, selectTitleFilter } from '../../redux/slices(modern appr)/filterSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 function Filter() {
+  const dispatch = useDispatch()
+  const titleFilter = useSelector(selectTitleFilter)
+
+  const handleTitleFilterChange = (e) => {
+    dispatch(setTitleFilter(e.target.value))
+  }
   return (
     <div className='app-block filter'>
-      <h2>Filters</h2>
+      <div className='filter-group'>
+        <input
+          value={titleFilter}
+          onChange={handleTitleFilterChange}
+          type='text'
+          placeholder='Filter by title...'
+        />
+      </div>
     </div>
   )
 }
--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import booksReducer from './books/reducer'
+import filterReducer from './slices(modern appr)/filterSlice'
 const store = configureStore({
   reducer: {
     books: booksReducer,
+    filter: filterReducer,
   },
 })
 
