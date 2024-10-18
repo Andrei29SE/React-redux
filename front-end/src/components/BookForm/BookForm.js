b0e37a0a7553e41043d31c70d823dbda5fc25f04
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-10-18 09:38:50 +0700

    22 - finished extrareducer for fetching data via API

--- front-end/src/components/BookForm/BookForm.js
+++ front-end/src/components/BookForm/BookForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { addBook, fetchBook } from '../../redux/slices(modern appr)/bookSlices'
+import { useDispatch, useSelector } from 'react-redux'
+import { addBook, fetchBook, selectBooksViaAPI } from '../../redux/slices(modern appr)/bookSlices'
 import './BookFrom.css'
 import booksData from '../../data/books.json'
 import createBookWithId from '../../utils/createBookWithId'
@@ -10,8 +10,9 @@ import { FaSpinner } from 'react-icons/fa'
 function BookForm() {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
-  //spinner state
-  const [isLoading, setIsLoading] = useState(false)
+  //spinner state(in Redux)
+  const isLoadingViaAPI = useSelector(selectBooksViaAPI)
+
   const dispatch = useDispatch()
   //add random book
   const handleOnRandom = () => {
@@ -28,18 +29,13 @@ function BookForm() {
       setTitle('')
       setAuthor('')
     } else {
-      dispatch(setError('Author and Title must be fullfielded'))
+      dispatch(setError('Fields Title and Author should be fulfilled!'))
     }
   }
 
   // add random book from API
-  const handleRandomBookAPI = async () => {
-    try {
-      setIsLoading(true)
-      await dispatch(fetchBook('http://localhost:4001/random-book-delayed'))
-    } finally {
-      setIsLoading(false)
-    }
+  const handleRandomBookAPI = () => {
+    dispatch(fetchBook('http://localhost:4001/random-book-delayed'))
   }
 
   return (
@@ -64,8 +60,8 @@ function BookForm() {
           Add Random Book
         </button>
 
-        <button type='button' onClick={handleRandomBookAPI} disabled={isLoading}>
-          {isLoading ? (
+        <button type='button' onClick={handleRandomBookAPI} disabled={isLoadingViaAPI}>
+          {isLoadingViaAPI ? (
             <>
               <span>Loading book...</span>
               <FaSpinner className='spinner' />
--- front-end/src/redux/slices(modern appr)/bookSlices.js
+++ front-end/src/redux/slices(modern appr)/bookSlices.js
@@ -2,55 +2,78 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import createBookWithId from '../../utils/createBookWithId'
 import axios from 'axios'
 import { setError } from './errorSlice'
-const initialState = []
 
-export const fetchBook = createAsyncThunk('bookManager/fetchBook', async (url, thunkAPI) => {
+const initialState = {
+  books: [],
+  isLoadingViaAPI: false,
+}
+
+// async fetching data from backend server
+export const fetchBook = createAsyncThunk('books/fetchBook', async (url, thunkAPI) => {
   try {
     const res = await axios.get(url)
     return res.data
   } catch (error) {
     thunkAPI.dispatch(setError(error.message))
-    throw error
+    // //Option 1
+    // throw error
+    // Option 2
+    return thunkAPI.rejectWithValue(error)
   }
 })
 
 const bookSlice = createSlice({
-  name: 'bookManager',
+  name: 'books',
   initialState: initialState,
   reducers: {
     addBook: (state, action) => {
-      return [...state, action.payload]
+      state.books.push(action.payload)
     },
     deleteBook: (state, action) => {
-      return state.filter((book) => book.id !== action.payload)
+      return { ...state, books: state.books.filter((book) => book.id !== action.payload) }
     },
     toggleFavourite: (state, action) => {
-      return state.map((book) =>
-        book.id === action.payload ? { ...book, isFavourite: !book.isFavourite } : book
-      )
+      state.books.forEach((book) => {
+        if (book.id === action.payload) {
+          book.isFavourite = !book.isFavourite
+        }
+      })
     },
   },
+
   // Thunkfunc in reducer (check out https://redux-toolkit.js.org/api/createAsyncThunk)
   // Option 1
   extraReducers: (builder) => {
     builder.addCase(fetchBook.fulfilled, (state, action) => {
-      if (action.payload.title && action.payload.author) {
-        state.push(createBookWithId(action.payload, 'API'))
+      if (action?.payload?.title && action?.payload?.author) {
+        state.books.push(createBookWithId(action.payload, 'API'))
+        state.isLoadingViaAPI = false
       }
+    })
+    builder.addCase(fetchBook.pending, (state) => {
+      state.isLoadingViaAPI = true
+    })
+    builder.addCase(fetchBook.rejected, (state) => {
+      state.isLoadingViaAPI = false
     })
   },
   // Option 2
-  // extraReducers: {[fetchBook.fulfilled]:(state, action) => {
-  //   if (action.payload.title && action.payload.author) {
-  //     state.push(createBookWithId(action.payload, 'API'))
-  //   }
-  // } }
+  // Doesn't work with this notation(have no idea why)
+  // extraReducers: {
+  //   [fetchBook.fulfilled]: (state, action) => {
+  //     if (action.payload.title && action.payload.author) {
+  //       state.books.push(createBookWithId(action.payload, 'API'))
+  //     }
+  //   },
+  // },
 })
 
 // reducers
 export const { addBook, deleteBook, toggleFavourite } = bookSlice.actions
 
 // Subscribe functions(selectors)
-export const selectBooks = (state) => state.books
+//
+export const selectBooks = (state) => state.books.books
+export const selectBooksViaAPI = (state) => state.books.isLoadingViaAPI
 
 export default bookSlice.reducer
